8625da33aa4bb1647e4f3a7e581a8497e8a27a35
Firzan97 <FirzanAzrai97@gmail.com>
2020-08-02 01:37:38 +0800

    initial commit

--- config/database.js
+++ config/database.js
@@ -4,13 +4,8 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
-        host: "${process.env.DATABASE_HOST}",
-        port: "${process.env.DATABASE_PORT}",
-        database: "${process.env.DATABASE_NAME}",
-        username: "${process.env.DATABASE_USERNAME}",
-        password: "${process.env.DATABASE_PASSWORD}",
-        ssl: { "rejectUnauthorized": false }
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
         useNullAsDefault: true,
