bd88596eb59f781e8501256ad6d4292b212e5bb6
Firzan97 <FirzanAzrai97@gmail.com>
2020-08-02 01:05:49 +0800

    deploying strapi for gatsby deployment

---
+++ config/plugins.js
@@ -0,0 +1,11 @@
+module.exports = ({ env }) => ({
+
+    upload: {
+        provider: "cloudinary",
+        providerOptions: {
+            cloud_name: "dcssh28oj",
+            api_key: "662887653386342",
+            api_secret: "o3-rK3GLO1T2sV9Zo1paAqjmeKU"
+        }
+    },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '63a649af-b3b9-4b32-b877-776c1014938a'
+};
